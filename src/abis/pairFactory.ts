1ce1ba6204f2a5e853f848161e5c7641e9fb4fd3
oxSaturn <oxSaturn@proton.me>
2023-10-12 13:59:38 +0800

    add removeLiquidity

--- src/abis/pairFactory.ts
+++ src/abis/pairFactory.ts
@@ -1,4 +1,4 @@
-export default [
+export const abi = [
     {
         "type": "constructor",
         "inputs": []
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import { createPublicClient, formatUnits, http } from "viem";
 import {
+  erc20ABI,
   useAccount,
   useConnect,
   useContractRead,
@@ -8,6 +9,7 @@ import {
   useNetwork,
   usePrepareContractWrite,
   useSwitchNetwork,
+  useWaitForTransaction,
 } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 
@@ -15,11 +17,11 @@ import { canto } from "viem/chains";
 
 import pairs from "../pairs.json";
 
-import gaugeAbi from "../abis/gauge";
-import pairAbi from "../abis/pair";
+import { abi as gaugeAbi } from "../abis/gauge";
+import { abi as pairAbi } from "../abis/pair";
+import { abi as routerAbi } from "../abis/router";
 
 import { WagmiConfig, createConfig } from "wagmi";
-
 const config = createConfig({
   autoConnect: true,
   publicClient: createPublicClient({
@@ -27,6 +29,8 @@ const config = createConfig({
     transport: http("https://mainnode.plexnode.org:8545"),
   }),
 });
+
+const router = "0x9B2920e72dF6E1A7053bEa7235c65079F5104398";
 export default function App() {
   return (
     <WagmiConfig config={config}>
@@ -47,7 +51,7 @@ export default function App() {
                   <td>{pair.symbol}</td>
                   <td>
                     {pair.pair}
-                    <BalanceOfPair pair={pair.pair} />
+                    <BalanceOfPair pair={pair.pair as `0x${string}`} />
                   </td>
                   <td>
                     <a
@@ -56,7 +60,7 @@ export default function App() {
                     >
                       {pair.gauge}
                     </a>
-                    <BalanceOfGauge gauge={pair.gauge} />
+                    <BalanceOfGauge gauge={pair.gauge as `0x${string}`} />
                   </td>
                 </tr>
               );
@@ -100,10 +104,12 @@ function BalanceOfGauge({ gauge: gaugeAddress }: { gauge: `0x${string}` }) {
   const { address, isConnected } = useAccount();
   const { data, isLoading } = useContractRead({
     address: gaugeAddress,
-    abi: gaugeAbi,
+    abi: erc20ABI,
     functionName: "balanceOf",
-    args: [address],
-    enabled: isConnected,
+    args: [address!],
+    enabled:
+      address !== undefined &&
+      gaugeAddress !== "0x0000000000000000000000000000000000000000",
   });
   if (!isConnected) return null;
   if (isLoading) return <div>loading</div>;
@@ -111,41 +117,168 @@ function BalanceOfGauge({ gauge: gaugeAddress }: { gauge: `0x${string}` }) {
     return (
       <div>
         balance in gauge: {formatUnits(data, 18)}
-        <WithdrawFromGauge gauge={gaugeAddress} amount={data as bigint} />
+        <WithdrawFromGauge gauge={gaugeAddress} />
       </div>
     );
+}
+
+function WithdrawFromGauge({ gauge: gaugeAddress }: { gauge: `0x${string}` }) {
+  const { address } = useAccount();
+  const { config } = usePrepareContractWrite({
+    address: gaugeAddress,
+    abi: gaugeAbi,
+    functionName: "withdrawAll",
+    enabled: address !== undefined,
+  });
+
+  const { write } = useContractWrite(config);
+
+  return <button onClick={() => write?.()}>Withdraw</button>;
 }
 
 function BalanceOfPair({ pair: pairAddress }: { pair: `0x${string}` }) {
   const { address, isConnected } = useAccount();
   const { data, isLoading } = useContractRead({
     address: pairAddress,
-    abi: pairAbi,
+    abi: erc20ABI,
     functionName: "balanceOf",
-    args: [address],
-    enabled: isConnected,
+    args: [address!],
+    enabled: address !== undefined,
   });
   if (!isConnected) return null;
   if (isLoading) return <div>loading</div>;
-  if (data) return <div>balance in pair: {formatUnits(data, 18)}</div>;
+  if (data)
+    return (
+      <div>
+        balance in pair: {formatUnits(data, 18)}
+        <WithdrawFromPair pair={pairAddress} amount={data} />
+      </div>
+    );
 }
 
-function WithdrawFromGauge({
-  gauge: gaugeAddress,
+function WithdrawFromPair({
+  pair: pairAddress,
   amount,
 }: {
-  gauge: `0x${string}`;
+  pair: `0x${string}`;
   amount: bigint;
 }) {
-  const { address, isConnected } = useAccount();
-  const { config } = usePrepareContractWrite({
-    address: gaugeAddress,
-    abi: gaugeAbi,
-    functionName: "withdrawAll",
-    enabled: isConnected,
+  const { address } = useAccount();
+
+  const { data: token0 } = useContractRead({
+    address: pairAddress,
+    abi: pairAbi,
+    functionName: "token0",
+    staleTime: Infinity,
   });
+
+  const { data: token1 } = useContractRead({
+    address: pairAddress,
+    abi: pairAbi,
+    functionName: "token1",
+    staleTime: Infinity,
+  });
+
+  const { data: stable } = useContractRead({
+    address: pairAddress,
+    abi: pairAbi,
+    functionName: "stable",
+    staleTime: Infinity,
+  });
+
+  const {
+    data: allowance,
+    refetch,
+    isLoading,
+  } = useContractRead({
+    address: pairAddress,
+    abi: erc20ABI,
+    functionName: "allowance",
+    args: [address!, router],
+    enabled: address !== undefined && !!router,
+  });
+
+  const isApproveNeeded = allowance !== undefined && allowance < amount;
+
+  const { config: approveConfig } = usePrepareContractWrite({
+    address: pairAddress,
+    abi: erc20ABI,
+    functionName: "approve",
+    args: [router, amount],
+    enabled: address !== undefined && isApproveNeeded,
+  });
+
+  const { write: approveWrite, data: approveTx } =
+    useContractWrite(approveConfig);
+
+  const { data: transaction } = useWaitForTransaction({
+    hash: approveTx?.hash,
+    onSuccess: () => {
+      refetch?.();
+    },
+    enabled: approveTx !== undefined,
+  });
+
+  const { data, isLoading: quoting } = useContractRead({
+    address: router,
+    abi: routerAbi,
+    functionName: "quoteRemoveLiquidity",
+    args: [token0!, token1!, stable!, amount],
+    enabled:
+      address !== undefined &&
+      token0 !== undefined &&
+      token1 !== undefined &&
+      stable !== undefined,
+  });
+
+  const { config, error } = usePrepareContractWrite({
+    address: router,
+    abi: routerAbi,
+    functionName: "removeLiquidity",
+    args: [
+      token0!,
+      token1!,
+      stable!,
+      amount,
+      100n,
+      100n,
+      address!,
+      BigInt(Date.now()) / 1000n + 600n,
+    ],
+    chainId: canto.id,
+    enabled:
+      token0 !== undefined &&
+      token1 !== undefined &&
+      stable !== undefined &&
+      amount > 0n &&
+      data !== undefined &&
+      address !== undefined,
+  });
+
+  console.log(config, error);
 
   const { write } = useContractWrite(config);
 
-  return <button onClick={() => write?.()}>Withdraw</button>;
+  return (
+    <>
+      <button
+        onClick={() => {
+          if (isApproveNeeded) {
+            approveWrite?.();
+          } else {
+            write?.();
+          }
+        }}
+        disabled={
+          quoting ||
+          config.request === undefined ||
+          isLoading ||
+          error !== null ||
+          write === undefined
+        }
+      >
+        {isApproveNeeded ? "Approve to Withdraw" : "Withdraw All"}
+      </button>
+    </>
+  );
 }
